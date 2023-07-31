664594b9eca22739492e7884e5e465b40ee85ca3
BZ <zhangzishan119@gmail.com>
2023-08-01 00:47:25 +1200

    Switch bar chart library
    Switch bar chart library to syncfusion_flutter_charts

--- lib/tabs/barchart_tab.dart
+++ lib/tabs/barchart_tab.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:charts_common/src/common/color.dart' as charts_color;
 
 class BarChartSample extends StatefulWidget {
@@ -35,94 +35,25 @@ class _BarChartSampleState extends State<BarChartSample> {
   List<int> get quarter3Data => widget.quarter3Data;
   List<int> get quarter4Data => widget.quarter4Data;
 
-  late List<charts.Series> seriesList;
+  late List<Domains> data;
+  late TooltipBehavior _tooltip;
 
-  List<charts.Series<Domains, String>> _createData() {
-    final q1 = [
-      Domains('Evaluate, Direct and Monitor', quarter1Data[0]),
-      Domains('Align, Plan and Organise', quarter1Data[1]),
-      Domains('Build, Acquire and Implement', quarter1Data[2]),
-      Domains('Deliver, Service and Support', quarter1Data[3]),
-      Domains('Monitor, Evaluate and Assess', quarter1Data[4]),
+  @override
+  void initState() {
+    data = [
+      Domains('Evaluate, Direct and Monitor', widget.quarter1Data[0],
+          quarter2Data[0], quarter3Data[0], quarter4Data[0]),
+      Domains('Align, Plan and Organise', widget.quarter1Data[1],
+          quarter2Data[1], quarter3Data[1], quarter4Data[1]),
+      Domains('Build, Acquire and Implement', widget.quarter1Data[2],
+          quarter2Data[2], quarter3Data[2], quarter4Data[2]),
+      Domains('Deliver, Service and Support', widget.quarter1Data[3],
+          quarter2Data[3], quarter3Data[3], quarter4Data[3]),
+      Domains('Monitor, Evaluate and Assess', widget.quarter1Data[4],
+          quarter2Data[4], quarter3Data[4], quarter4Data[4]),
     ];
-    final q2 = [
-      Domains('Evaluate, Direct and Monitor', quarter2Data[0]),
-      Domains('Align, Plan and Organise', quarter2Data[1]),
-      Domains('Build, Acquire and Implement', quarter2Data[2]),
-      Domains('Deliver, Service and Support', quarter2Data[3]),
-      Domains('Monitor, Evaluate and Assess', quarter2Data[4]),
-    ];
-    final q3 = [
-      Domains('Evaluate, Direct and Monitor', quarter3Data[0]),
-      Domains('Align, Plan and Organise', quarter3Data[1]),
-      Domains('Build, Acquire and Implement', quarter3Data[2]),
-      Domains('Deliver, Service and Support', quarter3Data[3]),
-      Domains('Monitor, Evaluate and Assess', quarter3Data[4]),
-    ];
-    final q4 = [
-      Domains('Evaluate, Direct and Monitor', quarter4Data[0]),
-      Domains('Align, Plan and Organise', quarter4Data[1]),
-      Domains('Build, Acquire and Implement', quarter4Data[2]),
-      Domains('Deliver, Service and Support', quarter4Data[3]),
-      Domains('Monitor, Evaluate and Assess', quarter4Data[4]),
-    ];
-
-    return [
-      charts.Series<Domains, String>(
-          id: 'Domains',
-          domainFn: (Domains domains, _) => domains.domain,
-          measureFn: (Domains domains, _) => domains.score,
-          data: q1,
-          fillColorFn: (Domains domains, _) {
-            return charts.MaterialPalette.yellow.shadeDefault;
-          }),
-      charts.Series<Domains, String>(
-          id: 'Domains',
-          domainFn: (Domains domains, _) => domains.domain,
-          measureFn: (Domains domains, _) => domains.score,
-          data: q2,
-          fillColorFn: (Domains domains, _) {
-            return charts.MaterialPalette.deepOrange.shadeDefault;
-          }),
-      charts.Series<Domains, String>(
-          id: 'Domains',
-          domainFn: (Domains domains, _) => domains.domain,
-          measureFn: (Domains domains, _) => domains.score,
-          data: q3,
-          fillColorFn: (Domains domains, _) {
-            return charts.MaterialPalette.blue.shadeDefault;
-          }),
-      charts.Series<Domains, String>(
-          id: 'Domains',
-          domainFn: (Domains domains, _) => domains.domain,
-          measureFn: (Domains domains, _) => domains.score,
-          data: q4,
-          fillColorFn: (Domains domains, _) {
-            return charts.MaterialPalette.gray.shadeDefault;
-          })
-    ];
-  }
-
-  barChart() {
-    return charts.BarChart(
-      _createData(),
-      vertical: true,
-      primaryMeasureAxis: charts.NumericAxisSpec(
-        renderSpec: charts.GridlineRendererSpec(
-          labelStyle: charts.TextStyleSpec(fontSize: 12),
-          lineStyle: charts.LineStyleSpec(
-            color: charts.ColorUtil.fromDartColor(Colors.transparent),
-          ),
-        ),
-        showAxisLine: false,
-        tickProviderSpec: charts.BasicNumericTickProviderSpec(
-          desiredTickCount: 10,
-        ),
-        // Set the maximum value of the y-axis to 270
-        viewport: charts.NumericExtents(0, 270),
-      ),
-      behaviors: [],
-    );
+    _tooltip = TooltipBehavior(enable: true);
+    super.initState();
   }
 
   // Helper function to create a single legend item
@@ -158,22 +89,50 @@ class _BarChartSampleState extends State<BarChartSample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            customLegend(),
-            Expanded(child: barChart()),
-          ],
-        ),
-      ),
-    );
+        body: Center(
+            child: Container(
+                child: Column(children: [
+      customLegend(),
+      Expanded(
+          child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              tooltipBehavior: _tooltip,
+              series: <ChartSeries<Domains, String>>[
+            ColumnSeries<Domains, String>(
+                dataSource: data,
+                xValueMapper: (Domains data, _) => data.domain,
+                yValueMapper: (Domains data, _) => data.q1Score,
+                color: Colors.yellow,
+                name: 'Q1'),
+            ColumnSeries<Domains, String>(
+                dataSource: data,
+                xValueMapper: (Domains data, _) => data.domain,
+                yValueMapper: (Domains data, _) => data.q2Score,
+                color: Colors.deepOrange,
+                name: 'Q2'),
+            ColumnSeries<Domains, String>(
+                dataSource: data,
+                xValueMapper: (Domains data, _) => data.domain,
+                yValueMapper: (Domains data, _) => data.q3Score,
+                color: Colors.lightBlue,
+                name: 'Q3'),
+            ColumnSeries<Domains, String>(
+                dataSource: data,
+                xValueMapper: (Domains data, _) => data.domain,
+                yValueMapper: (Domains data, _) => data.q4Score,
+                color: Colors.grey,
+                name: 'Q4')
+          ]))
+    ]))));
   }
 }
 
 class Domains {
   final String domain;
-  final int score;
+  final int q1Score;
+  final int q2Score;
+  final int q3Score;
+  final int q4Score;
 
-  Domains(this.domain, this.score);
+  Domains(this.domain, this.q1Score, this.q2Score, this.q3Score, this.q4Score);
 }
